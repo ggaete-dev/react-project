0628b7788ae0c7fdfa564173fcacc4e8273eb4b7
ggaete-dev <gastongaetedev@gmail.com>
2024-08-27 16:22:02 -0400

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, useRoutes } from "react-router-dom"
 import Home from "./pages/Home"
 import MyAccount from "./pages/MyAccount"
 import MyOrder from "./pages/MyOrder"
@@ -5,17 +6,25 @@ import MyOrders from "./pages/MyOrders"
 import NotFound from "./pages/NotFound"
 import SignIn from "./pages/SignIn"
 
+function AppRoutes() {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*',  element: <NotFound /> },
+  ])
+  
+  return routes;
+}
+
 function App() {
 
   return (
-    <div>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-      <NotFound />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
