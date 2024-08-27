673056698cf9583f375d3c3c3ae04a92c5fe5624
ggaete-dev <gastongaetedev@gmail.com>
2024-08-27 00:15:01 -0400

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import Home from "./pages/Home"
+import MyAccount from "./pages/MyAccount"
+import MyOrder from "./pages/MyOrder"
+import MyOrders from "./pages/MyOrders"
+import NotFound from "./pages/NotFound"
+import SignIn from "./pages/SignIn"
 
 function App() {
 
   return (
-    <div className="bg-red-100">
-      Hello world!
+    <div>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <SignIn />
+      <NotFound />
     </div>
   )
 }
